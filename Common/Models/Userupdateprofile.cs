4ae8fe5007a3698eeadf60dc8331af52dfe9d6ad
KathulaAbhiram <kmv.abhiram@gmail.com>
2024-05-10 10:03:51 +0530

    Updateprofile

---
+++ Common/Models/Userupdateprofile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Git one
+namespace login.Common.Models
+{
+    public class Userupdateprofile
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
