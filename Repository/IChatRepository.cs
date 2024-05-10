82dc53314c8eea7859508ed9f5cdaaa1bd5102e9
KathulaAbhiram <kmv.abhiram@gmail.com>
2024-05-10 20:04:15 +0530

    ichatrepo changed

--- Repository/IChatRepository.cs
+++ Repository/IChatRepository.cs
@@ -11,5 +11,7 @@ namespace Repository
         Task AddChatAsync(Chat chat);
         // Other method signatures as needed
         Task<IEnumerable<Chat>> getIndividualChats(String senderId, String receiverId);
+
+
     }
 }
