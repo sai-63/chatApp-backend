908168c3268050b25fda0212340b7f763dbc931e
KathulaAbhiram <kmv.abhiram@gmail.com>
2024-05-10 20:07:30 +0530

    tmodeltgrprepo

---
+++ Service/GroupService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using login.Common.Models;
+using Repository;
+
+namespace Service
+{
+    public class GroupService : IGroupService
+    {
+        private readonly IGroupRepository _groupRepository;
+
+        public GroupService(IGroupRepository groupRepository)
+        {
+            _groupRepository = groupRepository;
+        }
+        public async Task<Group> GetGroupByNameAsync(string groupName)
+        {
+            return await _groupRepository.GetGroupByNameAsync(groupName);
+        }
+
+        public async Task CreateGroupAsync(Group group)
+        {
+            await _groupRepository.CreateGroupAsync(group);
+        }
+
+        public async Task AddUsersToGroupAsync(string groupId, List<string> userIds)
+        {
+            await _groupRepository.AddUsersToGroupAsync(groupId, userIds);
+        }
+
+        //Get all groups
+        public async Task<IEnumerable<Group>> GetAllGroups()
+        {
+            return await _groupRepository.GetAllGroupsAsync();
+        }
+    }
+}
---
+++ Service/IGroupService.cs
@@ -0,0 +1,17 @@
+﻿using login.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface IGroupService
+    {
+        Task<Group> GetGroupByNameAsync(string groupName);
+        Task CreateGroupAsync(Group group);
+        Task AddUsersToGroupAsync(string groupId, List<string> userIds);
+        Task<IEnumerable<Group>> GetAllGroups();
+    }
+}
