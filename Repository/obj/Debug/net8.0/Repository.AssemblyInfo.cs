291d4427ee3842dbba6e41c3c22c1e237327466e
Baddam-Supreeth-Reddy <baddamsupreethreddy@gmail.com>
2024-05-14 21:14:22 +0530

    Remove from grp

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de0c7f97de080828c9a6997a3617c3ec724df83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794e2817808726acc40b567790ac52cd89336f2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
