3a8a3c386d1cf663a63a2bbdf70c304ab317a90a
KathulaAbhiram <kmv.abhiram@gmail.com>
2024-05-14 15:33:50 +0530

    Joingrp added

---
+++ Common/Models/Joingrp.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class Joingrp
+    {
+        [BsonId]
+        [BsonElement("username")]
+        public string username { get; set; }
+
+        [BsonElement("groupname")]
+        public string groupname { get; set; }
+    }
+}
