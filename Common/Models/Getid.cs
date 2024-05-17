87c476f186dc8bc181fe00636707531b5ed81f4e
KathulaAbhiram <kmv.abhiram@gmail.com>
2024-05-17 22:31:37 +0530

    GEtid added

---
+++ Common/Models/Getid.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class Getid
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public String Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("users")]
+        public List<string> Users { get; set; }
+
+        [BsonElement("messages")]
+        public List<New> Messages { get; set; }
+    }
+}
